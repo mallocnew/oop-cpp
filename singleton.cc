0afe739d1b6322eec9dacd49780d5fe7a65ddb6c
jok <easytojoin@163.com>
2024-04-23 20:11:51 +0800

    cpp: singleton

---
+++ singleton.cc
@@ -0,0 +1,59 @@
+// Copyright 2024 JOK Inc. All Rights Reserved.
+// Author: easytojoin@163.com (jok)
+
+#include <iostream>
+#include <thread>  // NOLINT
+#include <mutex>  // NOLINT
+
+class Singleton {
+ public:
+  static Singleton* getInstance() {
+    if (instance_ == nullptr) {
+      instance_ = new Singleton();
+    }
+    return instance_;
+  }
+
+ protected:
+  Singleton() { std::cout << "New instance: " << this << "\n"; }
+
+ private:
+  static Singleton* instance_;
+
+  Singleton(Singleton&) = delete;
+  Singleton& operator=(const Singleton&) = delete;
+};
+
+Singleton* Singleton::instance_ = nullptr;
+
+class TSafeSingleton {
+ public:
+  static TSafeSingleton* getInstance() {
+    std::lock_guard<std::mutex> lock(m_);
+    if (instance_ == nullptr) {
+      instance_ = new TSafeSingleton();
+    }
+    return instance_;
+  }
+
+ protected:
+  TSafeSingleton() {
+    std::cout << "New instance: " << this << "\n";
+  }
+
+ private:
+  static TSafeSingleton* instance_;
+  static std::mutex m_;
+
+  TSafeSingleton(TSafeSingleton&) = delete;
+  TSafeSingleton& operator=(const TSafeSingleton&) = delete;
+};
+
+TSafeSingleton* TSafeSingleton::instance_ = nullptr;
+std::mutex TSafeSingleton::m_;
+
+int main(int argc, char** argv) {
+  Singleton* i = Singleton::getInstance();
+  TSafeSingleton* i2 = TSafeSingleton::getInstance();
+  return 0;
+}
